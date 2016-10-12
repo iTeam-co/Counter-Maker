d3686026f6dbe608c4b0715d993d599d02262c0f
Prs Hrn <parsa_hrn@yahoo.com>
2016-10-12 23:30:36 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ def main(m):
 		r.setex(_hash, max_time, int(msgs) + 1
     if m.text == '/start':
       r.sadd('cmaker:users', str(m.from_user.id))
-      bot.send_message(m.chat.id, 'Welcome to @CmakerBot\nThis bot will add counter to your messages.\nDev: @prsahrn\n Powered By: @Iteam_en')
+      bot.send_message(m.chat.id, 'Welcome to @CmakerBot\nThis bot will add counter to your messages.\nDev: @prsahrn\n Powered By: @Iteam_en') #change this
     else:
       msg = bot.forward_message(channel, m.chat.id, m.message_id)
       bot.forward_message(m.chat.id, channel, msg.message_id)
