28bd119c25e8aa7141a66662a1d84c161cbfe0d8
AmirSbss <mrahs@emeil.ir>
2016-10-12 23:48:33 +0330

    Update

--- bot.py
+++ bot.py
@@ -1,33 +1,37 @@
 import telebot
 import sys
 import redis
-channel = '@channelid'
+token = 'TOKEN HERE' #write your token here
+channel = '@channelid' #write your channel id here
 r = redis.Redis('localhost')
 reload(sys)
 sys.setdefaultencoding('utf8')
-bot = telebot.TeleBot('175532355:AAEX7Lv1qoFPZfD8iauXS8m8VngQdcIdkMQ')
+bot = telebot.TeleBot(token)
 blocklist = r.smembers('blocklist')
 def is_block(cid):
 	return str(cid) in blocklist
 @bot.message_handler(content_types=['text','sticker', 'photo', 'audio', 'video', 'contact', 'location', 'document', 'venue'])
 def main(m):
-	if is_block(m.from_user.id):
-		pass
+    try:
+        if is_block(m.from_user.id):
+	    pass
 	else:
-		_hash = "anti_flood:" + str(m.from_user.id)
-		msgs = 0
-		if r.get(_hash):
-			msgs = int(r.get(_hash))
+	    _hash = "anti_flood:" + str(m.from_user.id)
+	    msgs = 0
+	    if r.get(_hash):
+	  	msgs = int(r.get(_hash))
 		max_msgs = 2
 		max_time = 1
 		if msgs > max_msgs:
-			r.sadd('blocklist', str(m.from_user.id))
-			bot.send_message(m.from_user.id, 'You have been blocked!\nBot will not respond to you.\n<b>Reason:</b> FLOOD',parse_mode='HTML')
-		r.setex(_hash, max_time, int(msgs) + 1)
-		if m.text == '/start':
-			r.sadd('cmaker:users', str(m.from_user.id))
-			bot.send_message(m.chat.id, 'Welcome to @CmakerBot\nThis bot will add counter to your messages.\nDev: @prsahrn\n Powered By: @Iteam_en') #change this
-		else:
-			msg = bot.forward_message(channel, m.chat.id, m.message_id)
-			bot.forward_message(m.chat.id, channel, msg.message_id)
+		 	r.sadd('blocklist', str(m.from_user.id))
+		 	bot.send_message(m.from_user.id, 'You have been blocked!\nBot will not respond to you.\n<b>Reason:</b> FLOOD',parse_mode='HTML')
+		r.setex(_hash, max_time, int(msgs) + 1
+        if m.text == '/start':
+            r.sadd('cmaker:users', str(m.from_user.id))
+            bot.send_message(m.chat.id, '*Welcome to Counter Maker Bot*\n`This bot will add counter to your messages.`',parse_mode="Markdown")
+        else:
+            msg = bot.forward_message(channel, m.chat.id, m.message_id)
+            bot.forward_message(m.chat.id, channel, msg.message_id)
+    except Exception as e:
+	print(e)
 bot.polling(none_stop=True)
